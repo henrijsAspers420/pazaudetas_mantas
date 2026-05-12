66b842d80af6cdbae139b07ceec4a18c40c8fe61
henrijsAspers420 <emils.pastars@ovg.lv>
2026-05-12 15:53:06 +0300

    Pabeigts_UI

--- app.py
+++ app.py
@@ -10,11 +10,6 @@ def index():
 def add_item():
     return render_template('add_item.html')
 
-@app.route('/login')
-def login():
-    # You can create a login.html later!
-    return "<h1>Pieteikšanās lapa (Coming Soon)</h1><a href='/'>Atpakaļ uz sākumu</a>"
-
 if __name__ == '__main__':
     # debug=True automatically reloads the server when you save changes
     app.run(debug=True)
